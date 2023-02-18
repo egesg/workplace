6d88475c0810ab12b7279ec37b33310530f4952a
egesg <egekcmz1@outlook.com>
2023-02-18 05:18:20 -0500

    Creating Requests and Responses

--- buildDjango/chefsTable/littleLemon/views.py
+++ buildDjango/chefsTable/littleLemon/views.py
@@ -20,5 +20,25 @@ def menu(request):
 def home(request):
     path = request.path
     # return HttpResponse(path, content_type='text/html', charset='utf-8')
-    response = HttpResponse("This works!")
-    return response # run it, see the text displayed in the browser
+    scheme = request.path
+    method = request.method
+    address = request.META['REMOTE_ADDR']
+    user_agent = request.META['HTTP_USER_AGENT']
+    path_info = request.path_info
+
+    response = HttpResponse()
+    response.headers['Age'] = 20
+
+    msg = f"""
+            <br>Path: {path}
+            <br>Address: {address}
+            <br>Scheme: {scheme}
+            <br>Method: {method}
+            <br>User agent: {user_agent}
+            <br>Path info: {path_info}
+            <br> Response header: {response.headers}
+            """
+    # response = HttpResponse("This works!")
+    # return response # run it, see the text displayed in the browser
+    return HttpResponse(msg, content_type='text/html', charset='utf-8')
+
