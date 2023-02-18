4ab2682ef759db5c1e59b0bc9af3388023343745
egesg <egekcmz1@outlook.com>
2023-02-18 04:19:32 -0500

    project level and app level urls.py

--- buildDjango/chefsTable/chefsTable/urls.py
+++ buildDjango/chefsTable/chefsTable/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from littleLemon import views
+from littleLemon import views # or from . import views  
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- buildDjango/chefsTable/littleLemon/urls.py
+++ buildDjango/chefsTable/littleLemon/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . import path
+from . import path # dot (.) here indicates the same working directory as the file
+from . import views
 
 urlpatterns = [
     path("". views.home),
